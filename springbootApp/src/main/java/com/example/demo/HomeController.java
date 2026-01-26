0c705f698c4fd98c933d1f04ac1715557031a163
gymkathirza <gymkathirza@gmail.com>
2026-01-26 04:32:11 +0000

    Added Thyme template

--- springbootApp/src/main/java/com/example/demo/HomeController.java
+++ springbootApp/src/main/java/com/example/demo/HomeController.java
@@ -14,12 +14,18 @@ public class HomeController {
 
 
     @RequestMapping("/home")
-    public String home(){
-        return "home.html";
+    public String home(Model model){
+        model.addAttribute("founder", "Founded by Kathiresan Moorthy");
+        return "home";
     }
 
     @RequestMapping("/ping")
     public String healthCheck(){
         return "ping.html";
+    }
+
+    @RequestMapping("/contact")
+    public String contact(){
+        return "contact.html";
     }
 }
