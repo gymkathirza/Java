4550c4764320acda6ff6c0dafaddc345732cdce6
gymkathirza <gymkathirza@gmail.com>
2026-02-07 21:59:07 +0000

    Updated List REST end point

--- demoapp/src/main/java/com/xcitasoft/demoapp/UserController.java
+++ demoapp/src/main/java/com/xcitasoft/demoapp/UserController.java
@@ -1,8 +1,10 @@
 package com.xcitasoft.demoapp;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.xcitasoft.models.User;
 
 @RestController
@@ -10,10 +12,11 @@ import com.xcitasoft.models.User;
 public class UserController {
 
     @GetMapping("/list")
-    public User[] getUsers(){
-        return new User[] {
+    public ResponseEntity<User[]> getUsers(){
+        User[] users = new User[] {
             new User("Alice", "alice@example.com"),
             new User("Bob", "bob@example.com")
         };
+        return ResponseEntity.ok(users);
     }
 };
